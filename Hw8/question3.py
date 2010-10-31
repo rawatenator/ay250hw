5f40ceebb7b8f9497f559730fc29bfd34e151f96
rawatenator <rawatenator@gmail.com>
2010-10-30 22:47:23 -0700

    still trying to configure hw5.py so that it can access the files in its dir

--- Hw8/question3.py
+++ Hw8/question3.py
@@ -1,9 +1,14 @@
+#import stuff/set sys.path for Hw5.Hw5
 import os
 import sys
+relative_path_to_module = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(os.path.abspath(relative_path_to_module))
+from Hw5.Hw5 import *
 
 # Download the latest polling data 
 # from http://electoral-vote.com/evp2010/Senate/ 
 # senate_polls.raw (or .csv) into your Week 8 homework folder.
+#-Done
   
 # Without moving the base code from your Week 5 (databases) 
 # folder*, load the necessary functions from that code into a new 
@@ -21,11 +26,3 @@ import sys
 #Include the necessary checks to make sure the user's 
 # path is set correctly to import the code from Week 5. 
 
-
-relative_path_to_module = os.path.join(os.path.dirname(__file__), '..')
-sys.path.append(os.path.abspath(relative_path_to_module))
-
-print sys.path
-
-from Hw5.Hw5 import *
-print 1
