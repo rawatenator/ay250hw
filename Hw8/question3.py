a4cb532fbb439f7c35a681aec58e05f9e72564a1
rawatenator <rawatenator@gmail.com>
2010-10-30 22:03:05 -0700

    broke question3 description into specific tasks

--- Hw8/question3.py
+++ Hw8/question3.py
@@ -1,14 +1,21 @@
 import os
-# 
-# !3.!Elections are coming up, and we want to re-use our code to 
-# see how things are going!  Download the latest polling data 
+
+# Download the latest polling data 
 # from http://electoral-vote.com/evp2010/Senate/ 
-# senate_polls.raw (or .csv) into your Week 8 homework folder.  
+# senate_polls.raw (or .csv) into your Week 8 homework folder.
+  
 # Without moving the base code from your Week 5 (databases) 
 # folder*, load the necessary functions from that code into a new 
-# module for your Week 8 folder which parses command-line 
-# input using argparse.  Allow the user to input new polls into 
-# the database without requiring a speciﬁc format.  Include an 
-# option which shows a plot of the updated polls for a given 
-# state.  Include the necessary checks to make sure the user’s 
+# module
+
+#which parses command-line 
+# input using argparse.  
+
+#Allow the user to input new polls into 
+# the database without requiring a speciﬁc format.  
+
+#Include an option which shows a plot of the updated polls for a given 
+# state.  
+
+#Include the necessary checks to make sure the user’s 
 # path is set correctly to import the code from Week 5. 
